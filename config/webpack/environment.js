49bbcc2716b04d4ec8a1f03956193335d6045e46
secondjobwork <secondjobwork@gmail.com>
2019-06-22 14:03:26 +0900

    webpackの設定変更

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,4 +4,14 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
+const alias = {
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+}
+environment.config.merge(alias)
+
 module.exports = environment
