b20e46c5b1749936cb993ba8eb8d860e865bcd78
secondjobwork <52063554+secondjobwork@users.noreply.github.com>
2019-06-22 14:05:56 +0900

    サンプルページとサンプルコンポーネントの作成 (#4)

---
+++ app/javascript/Hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+  created() {
+    this.message = this.data.message
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
